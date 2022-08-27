de9d2f6af3065f76ecf8bd257924a774eaab888a
JeremyF <jfulbright@gmail.com>
2022-08-27 15:05:34 -0600

    Added functionality where password is generated from alpha and converted uppercase array to new lowercase array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,41 @@
 
-// Assignment Code: Locates the button with class #generate". Creates a variable and stores the button object's class and id values "button#generate.btn". This is used to add an event listener on the button click event.
+// Assigns DOM location of Form fields and Button as variables. 
 var generateBtn = document.querySelector("#generate");
-  // Declare userInputs variables
-  var isSpecialChar = true;
-  var isNumeric = true;
-  var isMixedCase = true;
-  var passwordLength = 8;
+var passwordLengthUserInput = document.querySelector("#passwordLength");
+
+// Declare alpha, numeric and special character arrays
+const specialCharSet = ['!', '"', '#', '$', '%', '&', '\'', '(', '\'', ')', '*', '+', ',' , '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "\\", '\]',  '^', '_', '`', '{', '|', '}', '~', ']', ];
+const numericSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const alphaUpperCaseSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const alphaLowerCaseSet = alphaUpperCaseSet.map(Element => {
+  return Element.toLowerCase();
+});
 
 
-// Write password to the #password input
+// Declare userInputs values to be used as arguments in password function
+var isSpecialChar = true;
+var isNumeric = true;
+var isMixedCase = true;
+
+// Add event listener to the button that invokes 'writePassword' function when clicked.
+generateBtn.addEventListener("click", writePassword);
+
+// Display generated password value in the form's text field
 function writePassword() {
-  var password = generateRandomPassword(); //returns random password from special chars function
+  var passwordLength = passwordLengthUserInput.value; //on button click, save password length value to this variable
+  var password = generateRandomPassword(passwordLength); //invokes random password function and passes passwordLength value as an argument. Returns random password from special chars function
   var passwordText = document.querySelector("#password"); //stores location of password textarea in variable "textarea#password"
-
   passwordText.value = password; //sets the password value returned from "generatePassword" function to the password textarea
-
 }
 
-// Add event listener to the button that invokes the 'writePassword' function when clicked.
-generateBtn.addEventListener("click", writePassword);
-// TODO: add EventListeners for UserInputs
 
-
-
-
-
-
-// TODO: Store userInput in variables
-
-// Declare alpha, numeric and special character password arrays
-const specialCharSet = ['!', '"', '#', '$', '%', '&', '\'', '(', '\'', ')', '*', '+', ',' , '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "\\", '\]',  '^', '_', '`', '{', '|', '}', '~', ']', ];
-const alphabetSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const numericSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-
-
-
-// Returns RANDOM multiple special chars (Works)
-function  generateRandomPassword () {
+// Generate random special chars based. Arguments include Password Length, Include lowercase, uppercase, numeric, and/or special characters
+function  generateRandomPassword (l) {
   var passwordArray = []; // create an empty array
   
-  for (let i = 0; i < passwordLength; i++) {
-    var index = Math.floor(Math.random() * specialCharSet.length); // creates random index of special characters array for each loop
-    passwordArray.push(specialCharSet[index]); // pushes value of random index into the password array
+  for (let i = 0; i < l; i++) {
+    var index = Math.floor(Math.random() * alphabetUpperCaseSet.length); // creates random index of special characters array for each loop
+    passwordArray.push(alphabetUpperCaseSet[index]); // pushes value of random index into the password array
   }
   let password = passwordArray.join('');
   console.log(password);
@@ -52,6 +44,10 @@ function  generateRandomPassword () {
 }
 
 
+// Argument Logic:
+// if specialCharSet = true then concatinate array and run for loop;
+// else 
+// 
 
 
 
@@ -59,9 +55,12 @@ function  generateRandomPassword () {
 
 
 //TODO List:
-// 1. Generate random chars based in userInput
-// 2. Wireup to HTML Form
 
-// 3. Write logic for userInput CharType
-//    3.a How to combine multiple arrays into one ("if user selects input concatinate arrays")
-// 4. Wireup to HTML form
+
+// Write logic for userInput CharType
+// How to combine multiple arrays into one ("if user selects input concatinate arrays")
+// Wireup to HTML form
+
+
+// Error Handling
+// Password length check: onblur, value must be >= 8 && <=128
